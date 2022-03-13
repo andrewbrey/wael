477756b9658a29705e9253eedcdd6ba3f5e45a0f
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-13 15:52:57 -0700

    feat(*): init

---
+++ app/entry.server.tsx
@@ -0,0 +1,18 @@
+import { renderToString } from 'react-dom/server';
+import { RemixServer, type EntryContext } from 'remix';
+
+export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  const markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
+
+  responseHeaders.set('Content-Type', 'text/html');
+
+  return new Response('<!DOCTYPE html>' + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  });
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>WAEL</h1>
+    </div>
+  );
+}
