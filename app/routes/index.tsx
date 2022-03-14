8723a02edc2dd6cb103a4635c014c8765315bd6f
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-14 16:16:36 -0700

    feat(utils): add db connection logic

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,24 @@
+import { type LogEntry } from '@prisma/client';
+import { useLoaderData, type LoaderFunction } from 'remix';
+import { db } from '~/utils/db.server';
+
+type LoaderData = { logs: Array<LogEntry> };
+
+export const loader: LoaderFunction = async () => {
+  const logs = await db.logEntry.findMany();
+
+  const data: LoaderData = { logs };
+
+  return data;
+};
+
 export default function Index() {
+  const logs = useLoaderData<LoaderData>();
+
   return (
     <div>
       <h1>WAEL</h1>
+      <pre>{JSON.stringify(logs, null, 2)}</pre>
     </div>
   );
 }
