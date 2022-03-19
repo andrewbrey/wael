25758364d60d70ddf273e3c75bd533586afbe504
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-19 12:05:17 -0700

    feat(tailwind): add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,26 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, type MetaFunction } from 'remix';
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  type LinksFunction,
+  type MetaFunction,
+} from "remix";
+import tw from "~/styles/wael.css";
+
+export const links: LinksFunction = () => {
+  return [{ href: tw, rel: "stylesheet" }];
+};
 
 export const meta: MetaFunction = () => {
-  return { title: 'WAEL 🐋' };
+  return { title: "wael" };
 };
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="font-serif sm:text-blue-500 antialiased font-light bg-stone-100 text-stone-900">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
