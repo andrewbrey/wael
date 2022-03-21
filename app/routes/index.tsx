cb8ecbd7f610fcb6655517a6351aecc53b183cb2
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-20 22:04:49 -0700

    feat(stats): add stats to the home

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,53 @@
 import { type LogEntry } from "@prisma/client";
+import { formatRelative } from "date-fns";
 import { Link, type LoaderFunction } from "remix";
 import { ClientOnly } from "remix-utils";
 import { CSRChart, SSRChart } from "~/components/wael-chart";
 import { Table } from "~/components/wael-table";
 import { db } from "~/utils/db.server";
 import { json, useLoaderData } from "~/utils/io";
+import { currentStreak } from "~/utils/streak.server";
 
-type LoaderData = { entries: Array<LogEntry> };
+type LoaderData = { entries: Array<LogEntry>; latest?: LogEntry; streak: number };
 
 export const loader: LoaderFunction = async () => {
   const entries = await db.logEntry.findMany({ orderBy: { createdAt: "asc" } });
 
-  return json<LoaderData>({ entries });
+  const latest = entries.at(entries.length - 1);
+  const streak = currentStreak(entries, latest);
+
+  return json<LoaderData>({ entries, latest, streak });
 };
 
 export default function IndexRoute() {
   const data = useLoaderData<LoaderData>();
 
+  const fmtStreak = new Intl.NumberFormat().format(data.streak);
+
   return (
     <main className="relative flex h-full flex-col overflow-hidden">
-      <header className="pointer-events-none absolute top-4 right-4 select-none">
+      <header className="pointer-events-none absolute top-2 right-2 z-10 select-none bg-white p-2">
         <h1 className="text-right font-mono text-3xl font-bold tracking-widest text-gray-900 lg:text-5xl">
           <span>WA</span>
           <br />
           <span>EL</span>
         </h1>
       </header>
+
+      <aside className="absolute top-2 left-2 z-10 bg-white p-2 text-sm">
+        <dl className="space-y-3">
+          <div>
+            <dt className="text-gray-500">Last Log:</dt>
+            <dd className="capitalize text-gray-800">
+              {data.latest ? formatRelative(data.latest.createdAt, new Date()) : "Never"}
+            </dd>
+          </div>
+          <div>
+            <dt className="text-gray-500">Exercise Streak:</dt>
+            <dd className="break-words capitalize text-gray-800">{fmtStreak}</dd>
+          </div>
+        </dl>
+      </aside>
 
       <div className="relative m-4 max-h-[50%] flex-grow overflow-hidden md:max-h-[65%]">
         <ClientOnly fallback={<SSRChart />}>{() => <CSRChart data={data.entries} />}</ClientOnly>
---
+++ app/utils/streak.server.ts
@@ -0,0 +1,23 @@
+import { type LogEntry } from "@prisma/client";
+import { isSameDay, isToday, isYesterday, subDays } from "date-fns";
+
+export function currentStreak(entries: Array<LogEntry>, latest?: LogEntry) {
+  if (!latest) return 0;
+  if (!(isToday(latest.createdAt) || isYesterday(latest.createdAt))) return 0;
+  if (!(latest.cardio || latest.lift)) return 0;
+
+  let count = 1;
+  let cursor = latest.createdAt;
+
+  for (let i = entries.length - 1; i >= 0; i--) {
+    const { createdAt, cardio, lift } = entries[i];
+    if (isSameDay(createdAt, cursor)) continue;
+
+    if (isSameDay(createdAt, subDays(cursor, 1))) {
+      cursor = createdAt;
+      if (cardio || lift) count++;
+    }
+  }
+
+  return count;
+}
