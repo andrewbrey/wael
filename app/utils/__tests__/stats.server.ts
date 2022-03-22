c92bd658b3d98afdf061c0504568e789913b3bbe
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-22 14:36:23 -0700

    test(vitest): add vitest configuration and some test fixtures

---
+++ app/utils/__tests__/stats.server.ts
@@ -0,0 +1,24 @@
+import { type LogEntry } from "@prisma/client";
+import { readFile } from "fs-extra";
+import superjson from "superjson";
+import { expect, test } from "vitest";
+import { join } from "node:path";
+
+import { currentStreak, totalDays } from "~/utils/stats.server";
+
+interface Fixture {
+  entries: Array<LogEntry>;
+  latest: LogEntry;
+}
+
+test("stats for fixture file work", async () => {
+  const { entries, latest } = superjson.parse<Fixture>(
+    await readFile(join(__dirname, "fixture", "logs.json"), { encoding: "utf-8" })
+  );
+
+  const streak = currentStreak(entries, latest);
+  const total = totalDays(entries);
+
+  expect(streak).toBe(18);
+  expect(total).toBe(100);
+});
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    include: ["**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}", "**/__tests__/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./test/setup-test-env.ts"],
+  },
+});
