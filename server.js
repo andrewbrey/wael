15f2794a2f2bff10a31d103a851738de20cf4a3f
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-21 20:26:13 -0700

    feat(proxy): provide a way to use prisma studio in production if desired

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ import { createRequestHandler } from "@remix-run/express";
 import compression from "compression";
 import express from "express";
 import morgan from "morgan";
+import { spawn } from "node:child_process";
 import process from "node:process";
+
+let studioProcess;
 
 const app = express();
 
@@ -18,6 +21,32 @@ app.use("/build", express.static("public/build", { immutable: true, maxAge: "1y"
 // Everything else (like favicon.ico) is cached for an hour. You may want to be
 // more aggressive with this caching.
 app.use(express.static("public/", { maxAge: "1h" }));
+
+if (process.env.EXPOSE_PRISMA_STUDIO === "true") {
+  const port = process.env.PRISMA_STUDIO_PORT || 5555
+  const proxy = require("express-http-proxy");
+  const cheerio = require("cheerio");
+
+  studioProcess = spawn("npx", ["prisma", "studio", "--browser", "none", '--port', port], { stdio: "inherit" });
+
+  app.all(
+    "*",
+    proxy(`http://localhost:${port}`, {
+      filter: (req) => req.headers?.referer?.includes("__db") || req.url?.includes("__db"),
+      userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+        if (userReq.path === "/__db") {
+          const $ = cheerio.load(proxyResData);
+          $("html head").prepend('<base href="/__db/">');
+
+          return $.root().html();
+        }
+
+        return proxyResData;
+      },
+      proxyReqPathResolver: (req) => req.url.replace("/__db", ""),
+    })
+  );
+}
 
 app.use(morgan("tiny"));
 
@@ -41,6 +70,9 @@ process.on("SIGTERM", shutdown);
 
 function shutdown() {
   console.log("Shutting down server...");
+
+  studioProcess?.kill("SIGTERM");
+
   server?.close((err) => {
     if (err) console.log("Shutdown encountered error ", err);
 
