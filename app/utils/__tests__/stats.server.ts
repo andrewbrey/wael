0e77dc42ee8a18ff9273137629895cb5f45c4b64
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-25 11:49:37 -0700

    test(utils): use mock timers so fixture tests work

--- app/utils/__tests__/stats.server.ts
+++ app/utils/__tests__/stats.server.ts
@@ -1,7 +1,7 @@
 import { type LogEntry } from "@prisma/client";
 import { readFile } from "fs-extra";
 import superjson from "superjson";
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import { join } from "node:path";
 
 import { currentStreak, totalDays } from "~/utils/stats.server";
@@ -16,9 +16,14 @@ test("stats for fixture file work", async () => {
     await readFile(join(__dirname, "fixture", "logs.json"), { encoding: "utf-8" })
   );
 
+  vi.useFakeTimers();
+  vi.setSystemTime(latest.createdAt);
+
   const streak = currentStreak(entries, latest);
   const total = totalDays(entries);
 
   expect(streak).toBe(18);
   expect(total).toBe(100);
+
+  vi.useRealTimers();
 });
