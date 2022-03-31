a035f4b9161adc18798245e7209ac0709e19f1d2
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-31 10:14:27 -0700

    feat(react-18): upgrade to react 18

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -7,6 +7,9 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  // TODO: use `renderToReadableStream`? That export doesn't exist in `@types` but
+  // the React 18 upgrade docs recommend it...not sure what's goin on, so just
+  // using `renderToString` which still works, but in a more limited way
   const markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
 
   responseHeaders.set("Content-Type", "text/html");
